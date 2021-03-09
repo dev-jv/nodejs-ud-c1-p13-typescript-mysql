35d62638ba6c441f2328d48a91c5daca0eb35cbd
dev-jv <dev.javierleon@gmail.com>
2021-03-08 22:21:15 -0500

    four

--- dist/models/server.js
+++ dist/models/server.js
@@ -4,14 +4,22 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const user_1 = __importDefault(require("../routes/user"));
 class Server {
     constructor() {
+        this.apiPaths = {
+            users: '/api/users'
+        };
         this.app = express_1.default();
         this.port = process.env.PORT || '8000';
+        this.routes();
+    }
+    routes() {
+        this.app.use(this.apiPaths.users, user_1.default);
     }
     listen() {
         this.app.listen(this.port, () => {
-            console.log('Server running on port' + this.port);
+            console.log('Server running on port ' + this.port);
         });
     }
 }
---
+++ routes/user.ts
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+
+import { getUsers, getUser, postUser, putUser, deleteUser } from '../controller/users';
+
+const router = Router();
+
+router.get('/', getUsers);
+router.get('/', getUser);
+router.post('/', postUser);
+router.put('/', putUser);
+router.delete('/', deleteUser);
+
+export default router;
